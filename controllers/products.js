14c6692f58c6915dc31f0ff24ad478b8bf49cc24
unknown <orevaorior@gmail.com>
2023-05-01 00:31:57 +0100

    query added

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,13 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic =async(req, res)=>{
-    const product = await Product.find({})
-   return res.status(200).json({product});
+    const {featured} = req.query;
+    const objectQuery = {};
+    if(featured){
+        objectQuery.featured = featured === 'true' ? true : false;
+    }
+    const product = await Product.find(req.query)
+   return res.status(200).json({product, nbHits: product.length});
 }
 
 const getAllProducts =(req, res)=>{

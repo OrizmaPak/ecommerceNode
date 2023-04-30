e5d19c6d71c3275b55ca4752ddc271a737ad34a6
unknown <orevaorior@gmail.com>
2023-04-30 23:08:00 +0100

    controllers started

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,8 @@
-const Product = require('/models/product')
+const Product = require('../models/product')
 
 const getAllProductsStatic =async(req, res)=>{
-    await Product.find({})
-   return res.status(200).json(Product);
+    const product = await Product.find({})
+   return res.status(200).json({product});
 }
 
 const getAllProducts =(req, res)=>{

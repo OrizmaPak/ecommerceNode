4b73e90f9b83b9431d7677d17b28fb7793c791d7
unknown <orevaorior@gmail.com>
2023-05-01 02:33:04 +0100

    sort implemented

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,24 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic =async(req, res)=>{
-    const {featured} = req.query;
+    const {featured, company, name, sort} = req.query;
     const objectQuery = {};
     if(featured){
         objectQuery.featured = featured === 'true' ? true : false;
     }
-    const product = await Product.find(req.query)
-   return res.status(200).json({product, nbHits: product.length});
+    if(company){
+        objectQuery.company = company
+    }
+    if(name){
+        objectQuery.name = {$regex: name, $options: 'i'}
+    }
+    let result = Product.find(objectQuery)
+    if(sort){
+        let sorting = sort.split(',').join(' ')
+        result = result.sort(sorting)
+    }
+    const product = await result;
+   return res.status(200).json({product, nbHits: product.length}); 
 }
 
 const getAllProducts =(req, res)=>{
